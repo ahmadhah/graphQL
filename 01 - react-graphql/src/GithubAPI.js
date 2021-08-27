78100b06d540c4867402cd4b2b3617014d2bd8e2
ahmadhah <54240600+ahmadhah@users.noreply.github.com>
2021-08-27 18:17:39 +0500

    .

--- 01 - react-graphql/src/GithubAPI.js
+++ 01 - react-graphql/src/GithubAPI.js
@@ -1,7 +1,10 @@
 import { useEffect, useState } from 'react'
 import axios from 'axios'
 
+//* Fetch method hr browser support nhi krta,, so hmain axios use krni chahiye bcoz mostly browsers isko support krty
+
 const api = axios.create({
+    // ! base url is starting point of your API
     baseURL: 'https://api.github.com'
 });
 
@@ -12,9 +15,10 @@ function GithubAPI() {
 
     // componentDidMount
     useEffect(() => {
-
+        // ? hm asl me apna kam async fucntion me krna chah rhy,, ar hmain wo call bi krwana pry ga.. so hmain IIFE use krna pry ga
         // Immediately Invoked Function Expression - IIFE
         (async () => {
+            //! destruturing data and also renaming it,, ho skta data already variable apny khi use kiya ho..
             const { data: user } = await api.get('/users/uqutub')
             console.log('user ', user)
             setUserData(user);      // update state of the userData
@@ -23,12 +27,13 @@ function GithubAPI() {
             console.log('repos: ', repo)
             setUserRepos(repo);     // update state of the userRepos
         })()
-
     }, [])
 
     return (
         <div>
-            <pre>
+            {/* here we are using pre for presentaion of data*/}
+            <pre>      
+                {/* data show krwany k liye stringify me convert krna pra.. */}
                 {userData && JSON.stringify(userData, null, 4)}
             </pre>
             <hr />
--- 01 - react-graphql/src/GithubGraphQL.js
+++ 01 - react-graphql/src/GithubGraphQL.js
@@ -2,9 +2,13 @@ import { useEffect } from 'react'
 import axios from 'axios'
 
 const api = axios.create({
+    // ! base url is starting point of your API
     baseURL: 'https://api.github.com',
+    //? 
     headers: {
-        Authorization: `bearer ${process.env.REACT_APP_GITHUB_ACCESS_TOKEN}`
+        //* process.env me 1 object milta, ar us me tmam tokens jo apny store kiye hoty, wo hoty us me
+        Authorization: `bearer ${process.env.REACT_APP_GITHUB_ACCESS_TOKEN}` // agr token yha direct likh dain gy
+        //! to wo publically available ho jye ga,,  is liye .env file me rkhty      
     }
 });
 
@@ -31,12 +35,13 @@ function GithubAPI() {
     //             }
     //         }
     //     }`;
-
+    //     // ? graphql me 1 hi method use krty,,, Post ka
     //     const res = await api.post('/graphql', { query: QUERY_ORGANIZATION })
     //     console.log('GraphQL Response: ', res);
     // }
 
     const fetchData_ql = async (organizationName) => {
+        //todo Note : queryOrganization is our function , recieving some params
         const QUERY_ORGANIZATION = `query queryOrganization($organizationName: String!){
             organization(login: $organizationName) {
                 name
@@ -49,7 +54,7 @@ function GithubAPI() {
                 email
                 company
                 repositories(first: 5) {
-                    edges {
+                    edges { 
                         node {
                             name
                         }
@@ -58,13 +63,20 @@ function GithubAPI() {
             }
         }`;
 
-        const res = await api.post('/graphql', { query: QUERY_ORGANIZATION, variables: { organizationName } })
+        const res = await api.post('/graphql', {
+            query: QUERY_ORGANIZATION,
+            variables: {
+                organizationName //todo Note : jo name oper func parameter ko pas kiya tha same name hi yaha pass krain gy
+            }
+        })
         console.log('GraphQL Response: ', res);
     }
 
-    const addStart = async(repositoryId) => {
+// ! mutation kro,, add star kro,, ar usky bd,,hmain starrable ki body wala code la kr return krwa do
+
+    const addStart = async (repositoryId) => {
         const MUTATION_ADD_START = `mutation addStart($repositoryId: ID!) {
-            addStar(inout: {starrableId: $repositoryId}) {
+            addStar(input: {starrableId: $repositoryId}) {
                 starrable {
                     viewerHasStarred
                 }
@@ -76,10 +88,9 @@ function GithubAPI() {
     }
 
     useEffect(() => {
-        fetchData_ql("qutbITech")
+        fetchData_ql("qutbITech"),
+        // addStart('Paste here ID_OF_Repo, where you want to give star')
     }, [])
-
-
 
     return (
         <div>
